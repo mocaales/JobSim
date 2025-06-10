c43232d05336b8183e3f34de6334bf6326b3a69e
AM <HP2@MacBook-Air-7.local>
2025-06-10 22:29:08 +0200

    dunno anymore

--- JobSim/app.config.js
+++ JobSim/app.config.js
@@ -4,13 +4,6 @@ import appJson from './app.json';
 export default {
   expo: {
     ...appJson.expo,
-    updates: {
-      url: "https://u.expo.dev/2f34bcce-844b-47a9-9a92-b3ddef9e4bf2"
-    },
-    runtimeVersion: {
-      policy: "sdkVersion"
-    },
-
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL || 'http://10.0.2.2:5000/api',
       ...appJson.expo.extra
